c4412c60ba5309ae9217047bace42cb8732f58b8
fakeUser <fakeEmail233333@qq.com>
2022-08-22 15:33:56 +0800

    init commit

---
+++ crawler-service/src/main/java/com/crawler/nacos/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.crawler.nacos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello! This is a crawler node";
+    }
+
+}
