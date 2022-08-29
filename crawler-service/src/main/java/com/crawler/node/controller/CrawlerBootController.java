ec3393927b55145cd8309269161c430b88bf2d3f
smx-Morgan <1048492656@qq.com>
2022-08-29 19:30:15 +0800

    redis list write json

--- crawler-service/src/main/java/com/crawler/node/controller/CrawlerBootController.java
+++ crawler-service/src/main/java/com/crawler/node/controller/CrawlerBootController.java
@@ -1,6 +1,7 @@
 package com.crawler.node.controller;
 
 import com.crawler.api.entity.TaskWrapper;
+import com.crawler.node.store.UrlStorage;
 import com.crawler.spider.client.JDItemClient;
 import com.crawler.spider.processor.searchProcessor.JDSearchProcessor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,10 +22,13 @@ public class CrawlerBootController {
 
     @Resource
     StringRedisTemplate redis;
+    @Resource
+    UrlStorage urlStorage;
 
     @PostMapping("/search")
     @ResponseBody
     public void dispatchSearchTask(@RequestBody String wrapperJson) {
+
         accomplishTask(wrapperJson, (taskWrapper) ->
                 Spider.create(new JDSearchProcessor())
                         .addUrl(taskWrapper.getMetaData())
@@ -42,8 +46,9 @@ public class CrawlerBootController {
 
     void accomplishTask(String wrapperJson, Consumer<TaskWrapper> crawlerCode) {
         TaskWrapper task = TaskWrapper.fromJson(wrapperJson);
-        log.info("收到任务：{}", task);
 
+        log.info("收到任务：{}", task);
+        urlStorage.creatList(task.getMetaData());
         CompletableFuture.runAsync(() -> {
             crawlerCode.accept(task);
             redis.opsForHash().put("crawlerTask", task.toJson(), "true");
